bbba71ddc709f08ac0ed65ef3405041ed88b01bd
Andrew McMillion <amcmillion3@gmail.com>
2022-07-10 09:36:25 -0400

    Add styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,11 +6,13 @@ const images = importAll(require.context('../../assets/images', false, /\.(png|j
 
 function Header() {
     return(
-        <header className="flex justify-center items-center mt-10">
-            <div className="w-12 h-12">
-                <img src={`${images[0]}`} alt="pokeball" />
+        <header>
+            <div className="flex justify-center items-center pt-10 pb-5">
+                <div className="w-12 h-12 mr-4">
+                    <img src={`${images[0]}`} alt="pokeball" />
+                </div>
+                <h1 className="text-4xl md:text-6xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text text-center ml-4">Pokedex</h1>
             </div>
-            <h1 className="text-4xl md:text-6xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text text-center ml-2">Pokedex</h1>
         </header>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,15 +10,15 @@ function Home() {
   }, [])
 
   return (
-    <div>
-      <div className="flex flex-wrap">
+    <div className="">
+      <div className="flex flex-wrap justify-evenly">
         {pokemons.map(pokemon => {
             const poke_id = pokemon.url.split("/")[6];
             const padded_poke_id = poke_id.padStart(3, "0");
             return (
-            <div key={pokemon.name}>
+            <div className="flex flex-col justify-between items-center border-2 rounded border-slate-100 shadow-md hover:shadow-lg bg-white p-4 m-6" key={pokemon.name}>
                 <img src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/thumbnails-compressed/${padded_poke_id}.png`} alt={pokemon.name} />
-                <h3 key={pokemon.name}>{pokemon.name}</h3>
+                <h3 className="capitalize text-xl" key={pokemon.name}>{pokemon.name}</h3>
             </div>
             )
         })}
