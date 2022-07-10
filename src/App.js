2da6096e5e890bdf258b3737c18a2bc40e6bb1ce
Andrew McMillion <amcmillion3@gmail.com>
2022-07-10 09:22:19 -0400

    Add styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-gradient-to-r from-red-50 to-green-50 via-blue-50">
       <Header />
       <Home />
     </div>
