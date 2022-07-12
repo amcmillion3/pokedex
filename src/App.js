796f8b408863af173d5cb98c8ae22eb8371d82a4
Andrew McMillion <amcmillion3@gmail.com>
2022-07-11 22:06:06 -0400

    Add full screen background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-red-50 to-green-50 via-blue-50">
+    <div className="h-screen bg-gradient-to-r from-red-50 to-green-50 via-blue-50">
       <Header />
       <Home />
     </div>
